f186ad2b4346ef73aa29570ae27002ae3bf876e9
John Glancy <jeglancy@gmail.com>
2016-11-01 12:42:38 -0700

    feat: add cron job to send reminder email to users with email and active games       note: cron job submits on dev server but need to verify email is             actually properly formed

--- main.py
+++ main.py
@@ -6,26 +6,32 @@ import logging
 
 import webapp2
 from google.appengine.api import mail, app_identity
-from api import TicTacToeApi
+from google.appengine.ext import ndb
 
-from models import User
+from api import TicTacToeApi
+from models import User, Game
 
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
-        """Send a reminder email to each User with an email about games.
+        """Send a reminder email to each User with an email & incomplete game.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
         for user in users:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Tic Tac Toe!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
+            games = Game.query(Game.game_over == False,
+                               ndb.OR(Game.user_name_x == user.key,
+                                      Game.user_name_o == user.key))
+            if games:
+                subject = 'You have a game in progress'
+                body = """Hello {}, you still have a game in progress!
+                          Come back & finish up!""".format(user.name)
+                # This will send test emails, the arguments to send_mail are:
+                # from, to, subject, body
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                               user.email,
+                               subject,
+                               body)
 
 
 # class UpdateAverageMovesRemaining(webapp2.RequestHandler):
